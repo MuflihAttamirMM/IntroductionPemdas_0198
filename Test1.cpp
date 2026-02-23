9da7ac66efccd1bccbba6722217b9d655fc8016a
Muflay <muflihattamirmuslim@gmail.com>
2026-02-23 16:39:24 +0700

    first commit

---
+++ Test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//variable label
+string nama:
+
+int main(){ //program utama
+    int age; //variable local
+    int age1; //variable local
+    age = 20; 
+    age1 = 26;
+
+
+}
